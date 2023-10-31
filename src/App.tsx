a1bd4086eeca428808da92ae333110e53e933a7a
fzsf163 <yofaisol619@gmail.com>
2023-11-01 02:28:36 +0600

    drag control works

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Reorder, useDragControls } from "framer-motion";
 import "./App.css";
 import { imgList } from "./assets";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import ImgBox from "./components/ImageContainer";
 function App() {
   let imglist = [...imgList];
   const [items, setItems] = useState(imglist);
   const controls = useDragControls();
+  const constraintsRef = useRef(null)
 
   return (
     <section className="h-[100%] bg-bg-color text-text-color font-[system-ui]  text-center space-y-10 box-border">
@@ -18,7 +19,8 @@ function App() {
       <Reorder.Group
         values={items}
         onReorder={setItems}
-        className="grid grid-cols-4 justify-items-center items-center w-fit mx-auto border border-border-color rounded-md p-3"
+        className="grid grid-cols-5 justify-items-center items-center w-[80%] mx-auto border border-border-color rounded-md p-3 gap-5"
+        ref={constraintsRef}
       >
         {items.map((item) => {
           return (
@@ -27,17 +29,17 @@ function App() {
               key={item}
               drag
               dragControls={controls}
-              dragElastic={0.2}
-              className="relative w-[30dvh] h-fit first:w-[40dvh] m-3"
-              dragSnapToOrigin
+              dragElastic={0}
+              className="relative w-[30dvh] h-fit first:col-span-2 first:row-span-2 first:w-full "
+              dragSnapToOrigin={true}
+              dragConstraints={constraintsRef}
             >
               <ImgBox key={item} img={item}></ImgBox>
               <span
                 className="cursor-move select-none text-black p-6 bg-transparent absolute top-0 left-0 w-full h-full"
                 onPointerDown={(e) => controls.start(e)}
-              >
-                Drag
-              </span>
+                style={{ touchAction: "none" }}
+              ></span>
             </Reorder.Item>
           );
         })}
