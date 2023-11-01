e62701022c9da0ca6007a549e040ebefce36f719
fzsf163 <yofaisol619@gmail.com>
2023-11-02 00:46:48 +0600

    hover show checkbox and checked state

--- src/components/ImgBox.tsx
+++ src/components/ImgBox.tsx
@@ -5,13 +5,19 @@ type Props = {
 };
 export default function ImgBox({ img }: Props) {
   const [check, setCheck] = useState(false);
+  const [isHover, setHover] = useState(false);
   return (
-    <div>
+    <div className="relative">
       <motion.img
         onTap={() => setCheck(!check)}
+        whileTap={{
+          scale: 0.6,
+        }}
         initial={{
           opacity: 1,
         }}
+        onHoverStart={() => setHover(true)}
+        onHoverEnd={() => setHover(false)}
         whileHover={{
           opacity: 0.7,
           scale: 0.96,
@@ -22,14 +28,26 @@ export default function ImgBox({ img }: Props) {
         alt={img}
         className="rounded-md object-cover bg-white"
       ></motion.img>
-      <motion.span>
+      <motion.span
+        className="accent-gray-400 absolute top-0 left-0 m-4"
+        initial={{
+          opacity: 0,
+        }}
+        animate={{
+          opacity: isHover || check ? 1 : 0,
+        }}
+      >
         <input
-          readOnly
           checked={check}
           value={check ? 1 : 0}
+          onChange={() => setCheck(!check)}
           type="checkbox"
           name="imgChk"
           id="imgChk"
+          className="peer appearance-none shrink-0 w-6 h-6 border-2 rounded-full  border-blue-900  bg-white
+          focus:outline-none focus:ring-offset-0 focus:ring-1 focus:ring-blue-100
+          checked:bg-bg-color checked:border-2
+          disabled:border-steel-400 disabled:bg-steel-400"
         />
       </motion.span>
     </div>
