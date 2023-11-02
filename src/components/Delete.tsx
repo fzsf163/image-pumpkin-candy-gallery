09f8baa8293050bf359a3bc494120152a5da1a45
fzsf163 <yofaisol619@gmail.com>
2023-11-02 22:44:37 +0600

    dlt, upload, imgs icon added

---
+++ src/components/Delete.tsx
@@ -0,0 +1,33 @@
+export default function DeleteSvG() {
+  return (
+    <div>
+      <svg
+        fill="red"
+        width="40px"
+        height="40px"
+        viewBox="0 0 24.00 24.00"
+        xmlns="http://www.w3.org/2000/svg"
+        id="delete"
+        className="icon glyph w-4 h-4 md:w-8 md:h-8 lg:w-10 lg:h-10"
+        stroke="#000000"
+        strokeWidth="0.00024000000000000003"
+      >
+        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+        <g
+          id="SVGRepo_tracerCarrier"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          stroke="#f0d1d1"
+          strokeWidth="2.544"
+        >
+          <path d="M17,4V5H15V4H9V5H7V4A2,2,0,0,1,9,2h6A2,2,0,0,1,17,4Z"></path>
+          <path d="M20,6H4A1,1,0,0,0,4,8H5V20a2,2,0,0,0,2,2H17a2,2,0,0,0,2-2V8h1a1,1,0,0,0,0-2Z"></path>
+        </g>
+        <g id="SVGRepo_iconCarrier">
+          <path d="M17,4V5H15V4H9V5H7V4A2,2,0,0,1,9,2h6A2,2,0,0,1,17,4Z"></path>
+          <path d="M20,6H4A1,1,0,0,0,4,8H5V20a2,2,0,0,0,2,2H17a2,2,0,0,0,2-2V8h1a1,1,0,0,0,0-2Z"></path>
+        </g>
+      </svg>
+    </div>
+  );
+}
