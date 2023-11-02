d023ffa8034d574dca4dd55555b6d9f25e939a07
fzsf163 <yofaisol619@gmail.com>
2023-11-03 01:51:07 +0600

    text style change

--- src/components/UploadBtn.tsx
+++ src/components/UploadBtn.tsx
@@ -1,7 +1,7 @@
 export default function UploadBtn() {
   return (
     <div className="w-fit flex flex-grow  items-center justify-center bg-grey-lighter">
-      <label className="w-fit h-fit flex flex-col items-center justify-center px-4 py-6 bg-white/30 text-blue rounded-lg shadow-lg tracking-wide uppercase  cursor-pointer hover:bg-blue hover:text-white">
+      <label className="w-fit h-fit flex flex-col items-center justify-center px-4 py-6 bg-white/30 text-blue rounded-lg shadow-lg tracking-wide capitalize  cursor-pointer hover:bg-blue hover:text-white">
         <svg
           className="w-8 h-8"
           fill="wheat"
@@ -10,7 +10,7 @@ export default function UploadBtn() {
         >
           <path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
         </svg>
-        <span className="mt-2 text-slate-400 leading-normal text-xs md:text-xl xl:text-2xl">Select a file</span>
+        <span className="mt-2 text-slate-400 leading-normal text-xs md:text-xl xl:text-2xl">Add image</span>
         <input type="file" className="hidden" />
       </label>
     </div>
